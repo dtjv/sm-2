062c0e4714b526898b28fea0a499c780c7d8c2b2
David Valles <davidtjvalles@gmail.com>
2021-09-28 04:47:11 -0700

    Add test for rep value. Fix lint issues

--- tests/index-test.ts
+++ tests/index-test.ts
@@ -22,10 +22,10 @@ test('returns correct interval for rep > 2', async (t) => {
   item = sm2(item, SuperMemoQuality.PASS_WITH_DIFFICULTY)
   item = sm2(item, SuperMemoQuality.PASS_WITH_DIFFICULTY)
 
-  const prevRepInterval = item.repInterval
+  const previousRepInterval = item.repInterval
 
   item = sm2(item, SuperMemoQuality.PASS_WITH_DIFFICULTY)
-  t.same(item.repInterval, Math.ceil(prevRepInterval * item.easyFactor))
+  t.same(item.repInterval, Math.ceil(previousRepInterval * item.easyFactor))
 })
 
 test('a 4 quality rating does not change E-Factor', async (t) => {
@@ -58,10 +58,24 @@ test('returns rep and repInterval defaults for quality rating 2', async (t) => {
   t.same(item.repInterval, SuperMemoItemDefaults.repInterval)
 })
 
+test('increments rep for quality ratings >= 3', async (t) => {
+  let item: SuperMemoItem = { ...SuperMemoItemDefaults }
+  const reps: number[] = []
+
+  item = sm2(item, SuperMemoQuality.PASS_WITH_DIFFICULTY)
+  reps.push(item.rep)
+  item = sm2(item, SuperMemoQuality.PASS_WITH_HESITATION)
+  reps.push(item.rep)
+  item = sm2(item, SuperMemoQuality.PASS_WITH_PERFECT_RECALL)
+  reps.push(item.rep)
+
+  t.same(reps, [1, 2, 3])
+})
+
 test('limits E-Factor to 1.3 for E-Factor < 1.3', async (t) => {
   let item: SuperMemoItem = { ...SuperMemoItemDefaults }
 
-  // it takes 9 repetitions with rating of 3 to get an E-Factor < 1.3
+  // It takes 9 repetitions with rating of 3 to get an E-Factor < 1.3
   for (let i = 0; i < 9; i += 1) {
     item = sm2(item, SuperMemoQuality.PASS_WITH_DIFFICULTY)
   }
